0aa5c37c93bae6fc2b931eeadaf380056d1a8542
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-02-22 23:29:27 +0100

    Added the AuthController. Excluded FolderProfile.pubxml from gitignore

---
+++ LetsTalk.Server.API/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using LetsTalk.Server.Application.Models.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LetsTalk.Server.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    [HttpPost("login")]
+    public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
+    {
+        Console.WriteLine("Login post");
+
+        return Ok(new AuthResponse());
+    }
+}
---
+++ LetsTalk.Server.Application/Models/Identity/AuthRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace LetsTalk.Server.Application.Models.Identity;
+
+public class AuthRequest
+{
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+}

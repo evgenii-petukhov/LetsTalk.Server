34fd4ebf24d91c5f3ad09c6d81bf31e35a6ac229
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-02-25 00:35:43 +0100

    Configured LetsTalkDbContext: added default data for the AccountType table

---
+++ LetsTalk.Server.Persistence/Configuration/AccountTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using LetsTalk.Server.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace LetsTalk.Server.Persistence.Configuration;
+
+public class AccountTypeConfiguration : IEntityTypeConfiguration<AccountType>
+{
+    public void Configure(EntityTypeBuilder<AccountType> builder)
+    {
+        builder.HasData(
+            new AccountType
+            {
+                Id = 1,
+                Name = "Facebook"
+            },
+            new AccountType
+            {
+                Id = 2,
+                Name = "VK"
+            }
+        );
+    }
+}
---
+++ LetsTalk.Server.Persistence/Migrations/20230224233358_AccountTypeDefaultData.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace LetsTalk.Server.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AccountTypeDefaultData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AccountTypes",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Facebook" },
+                    { 2, "VK" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AccountTypes",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "AccountTypes",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}

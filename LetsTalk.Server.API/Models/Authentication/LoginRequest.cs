2b935f6e3ae267404f817998a404c5a6a2311eb6
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-02-25 18:25:52 +0100

    Removed unnecessary fields from the LoginRequest and LoginCommand models

--- LetsTalk.Server.API/Models/Authentication/LoginRequest.cs
+++ LetsTalk.Server.API/Models/Authentication/LoginRequest.cs
@@ -6,15 +6,5 @@ public class LoginRequest
 
     public string? Id { get; set; }
 
-    public string? Email { get; set; }
-
-    public string? Name { get; set; }
-
-    public string? PhotoUrl { get; set; }
-
-    public string? FirstName { get; set; }
-
-    public string? LastName { get; set; }
-
     public string? AuthToken { get; set; }
 }
--- LetsTalk.Server.Core/Features/Authentication/Commands/LoginCommand.cs
+++ LetsTalk.Server.Core/Features/Authentication/Commands/LoginCommand.cs
@@ -8,15 +8,5 @@ public class LoginCommand : MediatR.IRequest<LoginResponse>
 
     public string? Id { get; set; }
 
-    public string? Email { get; set; }
-
-    public string? Name { get; set; }
-
-    public string? PhotoUrl { get; set; }
-
-    public string? FirstName { get; set; }
-
-    public string? LastName { get; set; }
-
     public string? AuthToken { get; set; }
 }

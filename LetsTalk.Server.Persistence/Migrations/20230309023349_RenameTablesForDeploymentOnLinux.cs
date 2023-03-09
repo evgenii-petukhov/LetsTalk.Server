d6ef1a76a480daab54c648b91aab422d4bee951b
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-03-09 03:36:01 +0100

    Linux: renamed tables (to lower case)

---
+++ LetsTalk.Server.Persistence/Migrations/20230309023349_RenameTablesForDeploymentOnLinux.cs
@@ -0,0 +1,198 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LetsTalk.Server.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class RenameTablesForDeploymentOnLinux : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_AccountTypes_AccountTypeId",
+                table: "Accounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Accounts_RecipientId",
+                table: "Messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Messages_Accounts_SenderId",
+                table: "Messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Messages",
+                table: "Messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_AccountTypes",
+                table: "AccountTypes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Accounts",
+                table: "Accounts");
+
+            migrationBuilder.RenameTable(
+                name: "Messages",
+                newName: "messages");
+
+            migrationBuilder.RenameTable(
+                name: "AccountTypes",
+                newName: "accounttypes");
+
+            migrationBuilder.RenameTable(
+                name: "Accounts",
+                newName: "accounts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_SenderId",
+                table: "messages",
+                newName: "IX_messages_SenderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Messages_RecipientId",
+                table: "messages",
+                newName: "IX_messages_RecipientId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Accounts_AccountTypeId",
+                table: "accounts",
+                newName: "IX_accounts_AccountTypeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_messages",
+                table: "messages",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_accounttypes",
+                table: "accounttypes",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_accounts",
+                table: "accounts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_accounts_accounttypes_AccountTypeId",
+                table: "accounts",
+                column: "AccountTypeId",
+                principalTable: "accounttypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_messages_accounts_RecipientId",
+                table: "messages",
+                column: "RecipientId",
+                principalTable: "accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_messages_accounts_SenderId",
+                table: "messages",
+                column: "SenderId",
+                principalTable: "accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_accounts_accounttypes_AccountTypeId",
+                table: "accounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_messages_accounts_RecipientId",
+                table: "messages");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_messages_accounts_SenderId",
+                table: "messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_messages",
+                table: "messages");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_accounttypes",
+                table: "accounttypes");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_accounts",
+                table: "accounts");
+
+            migrationBuilder.RenameTable(
+                name: "messages",
+                newName: "Messages");
+
+            migrationBuilder.RenameTable(
+                name: "accounttypes",
+                newName: "AccountTypes");
+
+            migrationBuilder.RenameTable(
+                name: "accounts",
+                newName: "Accounts");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_messages_SenderId",
+                table: "Messages",
+                newName: "IX_Messages_SenderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_messages_RecipientId",
+                table: "Messages",
+                newName: "IX_Messages_RecipientId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_accounts_AccountTypeId",
+                table: "Accounts",
+                newName: "IX_Accounts_AccountTypeId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Messages",
+                table: "Messages",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_AccountTypes",
+                table: "AccountTypes",
+                column: "Id");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Accounts",
+                table: "Accounts",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_AccountTypes_AccountTypeId",
+                table: "Accounts",
+                column: "AccountTypeId",
+                principalTable: "AccountTypes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Accounts_RecipientId",
+                table: "Messages",
+                column: "RecipientId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Messages_Accounts_SenderId",
+                table: "Messages",
+                column: "SenderId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}

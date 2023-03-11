c44494c033491c56d05152f7167a285cad20be1b
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-03-11 16:30:54 +0100

    Linux: added a new (-b) parameter to rebuild.sh

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,17 @@
+branch="master"
+while getopts b: args
+do
+    case "${args}" in
+        b) branch=${OPTARG};;
+    esac
+done
+
 systemctl stop letstalk.service
 systemctl status letstalk.service --no-pager
 git reset --hard
-git checkout origin/master
-git pull origin master
+git fetch origin
+git checkout origin/${branch}
+git pull origin ${branch}
 cd LetsTalk.Server.API
 dotnet ef database update
 cd ..

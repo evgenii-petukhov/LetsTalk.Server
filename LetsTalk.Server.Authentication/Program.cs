a4e9e56c8ede1598b8c0e1403df2adc8ae27c2eb
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-03-17 21:00:01 +0100

    Microservices: added some code to run it on linux

--- LetsTalk.Server.Authentication/Program.cs
+++ LetsTalk.Server.Authentication/Program.cs
@@ -1,9 +1,12 @@
 using LetsTalk.Server.Authentication.Abstractions;
 using LetsTalk.Server.Authentication.Models;
 using LetsTalk.Server.Authentication.Services;
+using Microsoft.AspNetCore.HttpOverrides;
+using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("all", builder =>
@@ -18,6 +21,11 @@ builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
 builder.Services.AddTransient<IJwtService, JwtService>();
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
+builder.Services.Configure<ForwardedHeadersOptions>(options =>
+{
+    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
+    options.KnownProxies.Add(IPAddress.Parse("127.0.0.1"));
+});
 builder.WebHost
     .ConfigureKestrel(serverOptions =>
     {
@@ -34,6 +42,8 @@ builder.WebHost
 var app = builder.Build();
 
 app.UseCors("all");
+
+app.UseForwardedHeaders();
 
 app.UseGrpcWeb();
 

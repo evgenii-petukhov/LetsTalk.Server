051ab3a9819af5838b6cfffa7d6b44dbfd7f2327
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-03-19 17:43:31 +0100

    Microservices: removed unnecessary code

--- LetsTalk.Server.Authentication/Program.cs
+++ LetsTalk.Server.Authentication/Program.cs
@@ -1,12 +1,9 @@
 using LetsTalk.Server.Authentication.Abstractions;
 using LetsTalk.Server.Authentication.Models;
 using LetsTalk.Server.Authentication.Services;
-using Microsoft.AspNetCore.HttpOverrides;
-using System.Net;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("all", builder =>
@@ -21,29 +18,12 @@ builder.Services.AddGrpc();
 builder.Services.AddGrpcReflection();
 builder.Services.AddTransient<IJwtService, JwtService>();
 builder.Services.Configure<JwtSettings>(builder.Configuration.GetSection("JwtSettings"));
-builder.Services.Configure<ForwardedHeadersOptions>(options =>
-{
-    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-    options.KnownProxies.Add(IPAddress.Parse("127.0.0.1"));
-});
-builder.WebHost
-    .ConfigureKestrel(serverOptions =>
-    {
-        serverOptions.ConfigureEndpointDefaults(listenOptions =>
-        {
-
-        });
-    })
-    .ConfigureAppConfiguration((builderContext, config) =>
-    {
-        config.AddJsonFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json"), optional: false);
-    });
+builder.Configuration
+    .AddJsonFile(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json"), optional: false);
 
 var app = builder.Build();
 
 app.UseCors("all");
-
-app.UseForwardedHeaders();
 
 app.UseGrpcWeb();
 

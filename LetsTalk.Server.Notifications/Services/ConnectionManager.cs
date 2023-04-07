69ffff2d4e302aa883b0d3054f89fdc4dc26b221
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-04-07 07:51:03 +0200

    TextHtml: code clean-up

--- LetsTalk.Server.Notifications/Services/ConnectionManager.cs
+++ LetsTalk.Server.Notifications/Services/ConnectionManager.cs
@@ -18,22 +18,16 @@ public class ConnectionManager : IConnectionManager
         lock (_lock)
         {
             var isFound = _connectionIdAccountIdBy
-                .Any(x => x.Value.Contains(connectionId, StringComparer.OrdinalIgnoreCase));
+                .Any(x => x.Value.Contains(connectionId));
 
             if (isFound)
             {
                 var mapping = _connectionIdAccountIdBy
-                    .First(x => x.Value.Contains(connectionId, StringComparer.OrdinalIgnoreCase));
+                    .First(x => x.Value.Contains(connectionId));
 
-                var newValue = mapping.Value
-                    .Where(x => !string.Equals(x, connectionId, StringComparison.OrdinalIgnoreCase))
-                    .ToHashSet();
+                mapping.Value.RemoveWhere(x => string.Equals(x, connectionId, StringComparison.Ordinal));
 
-                if (newValue.Any())
-                {
-                    _connectionIdAccountIdBy[mapping.Key] = newValue;
-                }
-                else
+                if (!mapping.Value.Any())
                 {
                     _connectionIdAccountIdBy.Remove(mapping.Key);
                 }
@@ -51,7 +45,12 @@ public class ConnectionManager : IConnectionManager
             }
             else
             {
-                _connectionIdAccountIdBy.Add(accountId, new HashSet<string> { connectionId });
+                _connectionIdAccountIdBy.Add(
+                    accountId, 
+                    new HashSet<string>(StringComparer.Ordinal)
+                    {
+                        connectionId
+                    });
             }
         }
     }

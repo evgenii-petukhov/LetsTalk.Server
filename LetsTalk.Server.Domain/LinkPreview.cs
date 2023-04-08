473855c62a3cea338038f74ad1e1329e1019c527
Evgenii Petukhov <petrovich2k@yandex.ru>
2023-04-08 03:15:24 +0200

    Renamed the LinkPreview table, added index by Url

--- LetsTalk.Server.Domain/LinkPreview.cs
+++ LetsTalk.Server.Domain/LinkPreview.cs
@@ -1,7 +1,15 @@
-﻿namespace LetsTalk.Server.Domain;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
+namespace LetsTalk.Server.Domain;
+
+[Table("linkpreviews")]
+[Index(nameof(Url))]
 public class LinkPreview: BaseEntity
 {
+    [Required]
+    [Column(TypeName = "longtext")]
     public string? Url { get; set; }
 
     public string? Title { get; set; }

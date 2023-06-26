b82306725a10198621869cd1aba69c7e0a1e0832
Evgenii Petukhov <petukhoven@gmail.com>
2023-06-26 06:23:50 +0200

    Code clean-up

--- LetsTalk.Server.Persistence/Repositories/AccountRepository.cs
+++ LetsTalk.Server.Persistence/Repositories/AccountRepository.cs
@@ -4,143 +4,142 @@ using LetsTalk.Server.Persistence.DatabaseContext;
 using LetsTalk.Server.Persistence.Enums;
 using Microsoft.EntityFrameworkCore;
 
-namespace LetsTalk.Server.Persistence.Repositories
+namespace LetsTalk.Server.Persistence.Repositories;
+
+public class AccountRepository : GenericRepository<Account>, IAccountRepository
 {
-    public class AccountRepository : GenericRepository<Account>, IAccountRepository
+    public AccountRepository(LetsTalkDbContext context) : base(context)
     {
-        public AccountRepository(LetsTalkDbContext context) : base(context)
-        {
-        }
+    }
 
-        public Task<Account?> GetByExternalIdAsync(string externalId, AccountTypes accountTypes, CancellationToken cancellationToken = default)
-        {
-            return _context.Accounts
-                .SingleOrDefaultAsync(q => q.ExternalId == externalId && q.AccountTypeId == (int)accountTypes, cancellationToken: cancellationToken);
-        }
+    public Task<Account?> GetByExternalIdAsync(string externalId, AccountTypes accountTypes, CancellationToken cancellationToken = default)
+    {
+        return _context.Accounts
+            .SingleOrDefaultAsync(q => q.ExternalId == externalId && q.AccountTypeId == (int)accountTypes, cancellationToken: cancellationToken);
+    }
 
-        public Task<Account?> GetByIdIncludingFilesAsync(int id, CancellationToken cancellationToken = default)
-        {
-            return _context.Accounts
-                .Include(x => x.Image)
-                .Include(x => x!.Image!.File)
-                .SingleOrDefaultAsync(q => q.Id == id, cancellationToken: cancellationToken);
-        }
+    public Task<Account?> GetByIdIncludingFilesAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return _context.Accounts
+            .Include(x => x.Image)
+            .Include(x => x!.Image!.File)
+            .SingleOrDefaultAsync(q => q.Id == id, cancellationToken: cancellationToken);
+    }
 
-        public async Task<IReadOnlyList<AccountWithUnreadCount>> GetOtherAsync(int id, CancellationToken cancellationToken = default)
-        {
-            var lastMessageDates = _context.Messages
-                .Where(x => x.SenderId == id || x.RecipientId == id)
-                .GroupBy(x => x.RecipientId == id ? x.SenderId : x.RecipientId)
-                .Select(g => new
+    public async Task<IReadOnlyList<AccountWithUnreadCount>> GetOtherAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var lastMessageDates = _context.Messages
+            .Where(x => x.SenderId == id || x.RecipientId == id)
+            .GroupBy(x => x.RecipientId == id ? x.SenderId : x.RecipientId)
+            .Select(g => new
+            {
+                AccountId = g.Key,
+                LastMessageDate = g.Max(x => x.DateCreatedUnix)
+            });
+
+        var unreadMessageCounts = _context.Set<Account>()
+            .Where(account => account.Id != id)
+            .Join(
+                _context.Messages
+                    .Where(message => message.RecipientId == id && !message.IsRead),
+                account => account.Id,
+                message => message.SenderId,
+                (account, message) => new
                 {
-                    AccountId = g.Key,
-                    LastMessageDate = g.Max(x => x.DateCreatedUnix)
-                });
-
-            var unreadMessageCounts = _context.Set<Account>()
-                .Where(account => account.Id != id)
-                .Join(
-                    _context.Messages
-                        .Where(message => message.RecipientId == id && !message.IsRead),
-                    account => account.Id,
-                    message => message.SenderId,
-                    (account, message) => new
-                    {
-                        Account = account,
-                        Message = message
-                    })
-                .GroupBy(g => g.Account)
-                .Select(g => new
-                {
-                    AccountId = g.Key.Id,
-                    UnreadCount = g.Count()
-                });
-
-            return await _context.Accounts
-                .Where(account => account.Id != id)
-                .GroupJoin(lastMessageDates, x => x.Id, x => x.AccountId, (x, y) => new
-                {
-                    Account = x,
-                    LastMessageDates = y
+                    Account = account,
+                    Message = message
                 })
-                .SelectMany(
-                    x => x.LastMessageDates.DefaultIfEmpty(),
-                    (x, y) => new
-                    {
-                        x.Account,
-                        LastMessageDates = y
-                    })
-                .Select(x => new
+            .GroupBy(g => g.Account)
+            .Select(g => new
+            {
+                AccountId = g.Key.Id,
+                UnreadCount = g.Count()
+            });
+
+        return await _context.Accounts
+            .Where(account => account.Id != id)
+            .GroupJoin(lastMessageDates, x => x.Id, x => x.AccountId, (x, y) => new
+            {
+                Account = x,
+                LastMessageDates = y
+            })
+            .SelectMany(
+                x => x.LastMessageDates.DefaultIfEmpty(),
+                (x, y) => new
                 {
                     x.Account,
-                    x.LastMessageDates!.LastMessageDate
+                    LastMessageDates = y
                 })
-                .GroupJoin(unreadMessageCounts, x => x.Account.Id, x => x.AccountId, (x, y) => new
+            .Select(x => new
+            {
+                x.Account,
+                x.LastMessageDates!.LastMessageDate
+            })
+            .GroupJoin(unreadMessageCounts, x => x.Account.Id, x => x.AccountId, (x, y) => new
+            {
+                Account = x,
+                UnreadMessageCounts = y
+            })
+            .SelectMany(
+                x => x.UnreadMessageCounts.DefaultIfEmpty(),
+                (x, y) => new
                 {
-                    Account = x,
+                    AccountInfo = x.Account,
                     UnreadMessageCounts = y
                 })
-                .SelectMany(
-                    x => x.UnreadMessageCounts.DefaultIfEmpty(),
-                    (x, y) => new
-                    {
-                        AccountInfo = x.Account,
-                        UnreadMessageCounts = y
-                    })
-                .Select(x => new AccountWithUnreadCount
-                {
-                    Id = x.AccountInfo.Account.Id,
-                    FirstName = x.AccountInfo.Account.FirstName,
-                    LastName = x.AccountInfo.Account.LastName,
-                    PhotoUrl = x.AccountInfo.Account.PhotoUrl,
-                    AccountTypeId = x.AccountInfo.Account.AccountTypeId,
-                    LastMessageDate = x.AccountInfo.LastMessageDate,
-                    UnreadCount = x.UnreadMessageCounts!.UnreadCount,
-                    ImageId = x.AccountInfo.Account.ImageId
-                })
-                .ToListAsync(cancellationToken: cancellationToken);
-        }
+            .Select(x => new AccountWithUnreadCount
+            {
+                Id = x.AccountInfo.Account.Id,
+                FirstName = x.AccountInfo.Account.FirstName,
+                LastName = x.AccountInfo.Account.LastName,
+                PhotoUrl = x.AccountInfo.Account.PhotoUrl,
+                AccountTypeId = x.AccountInfo.Account.AccountTypeId,
+                LastMessageDate = x.AccountInfo.LastMessageDate,
+                UnreadCount = x.UnreadMessageCounts!.UnreadCount,
+                ImageId = x.AccountInfo.Account.ImageId
+            })
+            .ToListAsync(cancellationToken: cancellationToken);
+    }
 
-        public Task<Account?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
-        {
-            return _context.Accounts
-                .SingleOrDefaultAsync(account => account.Id == id, cancellationToken: cancellationToken);
-        }
+    public Task<Account?> GetByIdAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return _context.Accounts
+            .SingleOrDefaultAsync(account => account.Id == id, cancellationToken: cancellationToken);
+    }
 
-        public Task SetImageIdAsync(int accountId, int imageId, CancellationToken cancellationToken = default)
-        {
-            return _context.Accounts
-                .Where(account => account.Id == accountId)
-                .ExecuteUpdateAsync(x => x
-                    .SetProperty(account => account.PhotoUrl, (string?)null)
-                    .SetProperty(account => account.ImageId, imageId), cancellationToken: cancellationToken);
-        }
+    public Task SetImageIdAsync(int accountId, int imageId, CancellationToken cancellationToken = default)
+    {
+        return _context.Accounts
+            .Where(account => account.Id == accountId)
+            .ExecuteUpdateAsync(x => x
+                .SetProperty(account => account.PhotoUrl, (string?)null)
+                .SetProperty(account => account.ImageId, imageId), cancellationToken: cancellationToken);
+    }
 
-        public Task UpdateAsync(int accountId, string? firstName, string? lastName, string? email, CancellationToken cancellationToken = default)
-        {
-            return _context.Accounts
-                .Where(account => account.Id == accountId)
-                .ExecuteUpdateAsync(x => x
-                    .SetProperty(account => account.FirstName, firstName)
-                    .SetProperty(account => account.LastName, lastName)
-                    .SetProperty(account => account.Email, email), cancellationToken: cancellationToken);
-        }
+    public Task UpdateAsync(int accountId, string? firstName, string? lastName, string? email, CancellationToken cancellationToken = default)
+    {
+        return _context.Accounts
+            .Where(account => account.Id == accountId)
+            .ExecuteUpdateAsync(x => x
+                .SetProperty(account => account.FirstName, firstName)
+                .SetProperty(account => account.LastName, lastName)
+                .SetProperty(account => account.Email, email), cancellationToken: cancellationToken);
+    }
 
-        public Task UpdateAsync(int accountId, string? firstName, string? lastName, string? email, string? photoUrl, CancellationToken cancellationToken = default)
-        {
-            return _context.Accounts
-                .Where(account => account.Id == accountId)
-                .ExecuteUpdateAsync(x => x
-                    .SetProperty(account => account.FirstName, firstName)
-                    .SetProperty(account => account.LastName, lastName)
-                    .SetProperty(account => account.Email, email)
-                    .SetProperty(account => account.PhotoUrl, photoUrl), cancellationToken: cancellationToken);
-        }
+    public Task UpdateAsync(int accountId, string? firstName, string? lastName, string? email, string? photoUrl, CancellationToken cancellationToken = default)
+    {
+        return _context.Accounts
+            .Where(account => account.Id == accountId)
+            .ExecuteUpdateAsync(x => x
+                .SetProperty(account => account.FirstName, firstName)
+                .SetProperty(account => account.LastName, lastName)
+                .SetProperty(account => account.Email, email)
+                .SetProperty(account => account.PhotoUrl, photoUrl), cancellationToken: cancellationToken);
+    }
 
-        public Task<bool> IsAccountIdValidAsync(int id, CancellationToken cancellationToken = default)
-        {
-            return _context.Accounts
-                .AnyAsync(account => account.Id == id, cancellationToken: cancellationToken);
-        }
+    public Task<bool> IsAccountIdValidAsync(int id, CancellationToken cancellationToken = default)
+    {
+        return _context.Accounts
+            .AnyAsync(account => account.Id == id, cancellationToken: cancellationToken);
     }
 }

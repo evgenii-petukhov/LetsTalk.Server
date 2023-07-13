92992a5a9898dc22cd0e23ec19eb902630b8a811
Evgenii Petukhov <petukhoven@gmail.com>
2023-07-13 23:27:23 +0200

    Send picture: handle IOExcpetion, if file exists: generate a new name and try again

--- LetsTalk.Server.FileStorageService/Services/FileService.cs
+++ LetsTalk.Server.FileStorageService/Services/FileService.cs
@@ -19,22 +19,41 @@ public class FileService : IFileService
         return File.ReadAllBytesAsync(imagePath, cancellationToken);
     }
 
-    public async Task<string?> SaveDataAsync(byte[] data, FileTypes fileType, ImageRoles imageRole, CancellationToken cancellationToken = default)
+    public Task<string> SaveDataAsync(byte[] data, FileTypes fileType, ImageRoles imageRole, CancellationToken cancellationToken = default)
     {
-        string filename;
-        string filepath;
-        (filename, filepath) = _fileNameGenerator.Generate(fileType);
-        if (File.Exists(filepath))
-        {
-            (filename, filepath) = _fileNameGenerator.Generate(fileType);
-        }
-        await File.WriteAllBytesAsync(filepath, data, cancellationToken);
-        return filename;
+        return SaveDataWithUniqueNameAsyncRepeatableIfExistsAsync(data, () => _fileNameGenerator.Generate(fileType), cancellationToken);
     }
 
     public void DeleteFile(string filename, FileTypes fileType)
     {
         var path = _fileNameGenerator.GetFilePath(filename, fileType);
         File.Delete(path);
+    }
+
+    private static async Task<string> SaveDataWithUniqueNameAsyncRepeatableIfExistsAsync(
+        byte[] data,
+        Func<(string, string)> generateUniqueFilename,
+        CancellationToken cancellationToken = default)
+    {
+        var (filename, filepath) = generateUniqueFilename.Invoke();
+        try
+        {
+            await SaveDataWithUniqueNameAsync(data, filepath, cancellationToken);
+        }
+        catch (IOException)
+        {
+            (filename, filepath) = generateUniqueFilename.Invoke();
+            await SaveDataWithUniqueNameAsync(data, filepath, cancellationToken);
+        }
+        return filename;
+    }
+
+    private static async Task SaveDataWithUniqueNameAsync(
+        byte[] data,
+        string filepath,
+        CancellationToken cancellationToken = default)
+    {
+        using var stream = new FileStream(filepath, FileMode.CreateNew, FileAccess.Write, FileShare.Read);
+        await stream.WriteAsync(data, cancellationToken);
     }
 }

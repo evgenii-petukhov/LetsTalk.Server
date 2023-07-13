c6c07d1800556546f171a29095ff9043a98801ae
Evgenii Petukhov <petukhoven@gmail.com>
2023-07-14 00:18:38 +0200

    Send picture: LetsTalk.Server.ImageProcessor: added IHost implementation and service registration

---
+++ LetsTalk.Server.ImageProcessor/ImageProcessorServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace LetsTalk.Server.ImageProcessor;
+
+public static class ImageProcessorServiceRegistration
+{
+    public static IServiceCollection AddImageProcessorServices(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        return services;
+    }
+}
--- LetsTalk.Server.ImageProcessor/Program.cs
+++ LetsTalk.Server.ImageProcessor/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using KafkaFlow;
+using LetsTalk.Server.ImageProcessor;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+using var host = CreateDefaultBuilder().Build();
+using var serviceScope = host.Services.CreateScope();
+
+var provider = serviceScope.ServiceProvider;
+var bus = provider.CreateKafkaBus();
+
+await bus.StartAsync();
+await host.RunAsync();
+await bus.StopAsync();
+
+static IHostBuilder CreateDefaultBuilder()
+{
+    return Host.CreateDefaultBuilder()
+        .ConfigureAppConfiguration(app =>
+        {
+            var filename = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "appsettings.json");
+            app.AddJsonFile(filename, optional: false);
+        })
+        .ConfigureServices((context, services) => services.AddImageProcessorServices(context.Configuration))
+        .UseSerilog((context, loggerConfig) =>
+        {
+            loggerConfig
+                .ReadFrom.Configuration(context.Configuration)
+                .Enrich.FromLogContext()
+                .WriteTo.Console();
+        });
+}

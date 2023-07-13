4fbe9fcc7042d95a88a097530902eebba0432ba8
Evgenii Petukhov <petukhoven@gmail.com>
2023-07-14 01:39:05 +0200

    Send picture: LetsTalk.Server.ImageProcessor: added Kafka event handler

--- LetsTalk.Server.ImageProcessor/ImageProcessorServiceRegistration.cs
+++ LetsTalk.Server.ImageProcessor/ImageProcessorServiceRegistration.cs
@@ -1,5 +1,11 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using KafkaFlow;
+using LetsTalk.Server.Configuration;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using LetsTalk.Server.Persistence;
+using KafkaFlow.Serializer;
+using KafkaFlow.TypedHandler;
+using LetsTalk.Server.Configuration.Models;
 
 namespace LetsTalk.Server.ImageProcessor;
 
@@ -9,6 +15,32 @@ public static class ImageProcessorServiceRegistration
         this IServiceCollection services,
         IConfiguration configuration)
     {
+        var kafkaSettings = KafkaSettingsHelper.GetKafkaSettings(configuration);
+        services.AddPersistenceServices(configuration);
+        services.AddKafka(
+            kafka => kafka
+                .UseConsoleLog()
+                .AddCluster(
+                    cluster => cluster
+                        .WithBrokers(new[]
+                        {
+                                kafkaSettings.Url
+                        })
+                        .CreateTopicIfNotExists(kafkaSettings.ImageResizeRequest!.Topic, 1, 1)
+                        .AddConsumer(consumer => consumer
+                            .Topic(kafkaSettings.ImageResizeRequest.Topic)
+                            .WithGroupId(kafkaSettings.ImageResizeRequest.GroupId)
+                            .WithBufferSize(100)
+                            .WithWorkersCount(10)
+                            .AddMiddlewares(middlewares => middlewares
+                                .AddSerializer<JsonCoreSerializer>()
+                                .AddTypedHandlers(h => h.AddHandler<ImageResizeRequestHandler>())
+                            )
+                        )
+                )
+        );
+        services.Configure<KafkaSettings>(configuration.GetSection("Kafka"));
+
         return services;
     }
 }
---
+++ LetsTalk.Server.ImageProcessor/ImageResizeRequestHandler.cs
@@ -0,0 +1,13 @@
+﻿using KafkaFlow;
+using KafkaFlow.TypedHandler;
+using LetsTalk.Server.ImageProcessor.Models;
+
+namespace LetsTalk.Server.ImageProcessor;
+
+public class ImageResizeRequestHandler : IMessageHandler<ImageResizeRequest>
+{
+    public Task Handle(IMessageContext context, ImageResizeRequest message)
+    {
+        throw new NotImplementedException();
+    }
+}

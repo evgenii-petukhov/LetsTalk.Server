5a43186a30f248967aca5a16d6958961dcd0c872
Evgenii Petukhov <petukhoven@gmail.com>
2023-07-16 23:28:34 +0200

    Added a new batch file to launch all microservices at once

---
+++ RunAll.bat
@@ -0,0 +1,6 @@
+start LetsTalk.Server.API\publish\LetsTalk.Server.API.exe
+start LetsTalk.Server.Authentication\publish\LetsTalk.Server.Authentication.exe
+start LetsTalk.Server.FileStorage.Service\publish\LetsTalk.Server.FileStorage.Service.exe
+start LetsTalk.Server.ImageProcessing.Service\publish\LetsTalk.Server.ImageProcessing.Service.exe
+start LetsTalk.Server.LinkPreview\publish\LetsTalk.Server.LinkPreview.exe
+start LetsTalk.Server.Notifications\publish\LetsTalk.Server.Notifications.exe

c297337f6148d77ca79cb21235c4219f66005dc4
Evgenii Petukhov <petukhoven@gmail.com>
2023-08-11 07:01:42 +0200

    Notification handling refactoring: added logging

--- LetsTalk.Server.Notifications/Handlers/NotificationHandler.cs
+++ LetsTalk.Server.Notifications/Handlers/NotificationHandler.cs
@@ -9,14 +9,20 @@ public class NotificationHandler<T> : IMessageHandler<Notification<T>[]>
     where T : class
 {
     private readonly INotificationService _notificationService;
+    private readonly ILogger<NotificationHandler<T>> _logger;
 
-    public NotificationHandler(INotificationService notificationService)
+    public NotificationHandler(
+        INotificationService notificationService,
+        ILogger<NotificationHandler<T>> logger)
     {
         _notificationService = notificationService;
+        _logger = logger;
     }
 
     public Task Handle(IMessageContext context, Notification<T>[] notifications)
     {
+        _logger.LogInformation("{@notifications}", notifications);
+
         return Task.WhenAll(notifications.Select(notification => _notificationService.SendNotificationAsync(notification.RecipientId, notification.Message!, typeof(T).Name)));
     }
 }

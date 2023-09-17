130670a33f8e9f8b0fb651a40f94f9d0c7f6eac2
Evgenii Petukhov <petukhoven@gmail.com>
2023-09-17 11:55:31 +0200

    DDD: removed unused constructor

--- LetsTalk.Server.Domain/File.cs
+++ LetsTalk.Server.Domain/File.cs
@@ -17,10 +17,6 @@ public class File : BaseEntity
     {
     }
 
-    public File(int id): base(id)
-    {
-    }
-
     public File(string fileName, int fileTypeId)
     {
         FileName = fileName;

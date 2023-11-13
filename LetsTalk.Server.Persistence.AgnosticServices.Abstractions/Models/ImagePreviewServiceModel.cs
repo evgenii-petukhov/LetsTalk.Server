426e4fabcc01198475a05e98e9c1150a01108b5e
Evgenii Petukhov <petukhoven@gmail.com>
2023-11-13 16:24:59 +0100

    Agnostic services: removed unused properties

--- LetsTalk.Server.Persistence.AgnosticServices.Abstractions/Models/ImagePreviewServiceModel.cs
+++ LetsTalk.Server.Persistence.AgnosticServices.Abstractions/Models/ImagePreviewServiceModel.cs
@@ -2,11 +2,7 @@
 
 public class ImagePreviewServiceModel
 {
-    public int MessageId { get; set; }
-
     public int Id { get; set; }
-
-    public int AccountId { get; set; }
 
     public int? Width { get; set; }
 

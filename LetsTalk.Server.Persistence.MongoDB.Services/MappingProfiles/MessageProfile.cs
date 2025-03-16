867a76ac828a6f237245bfe5b1c065de0c8792c4
Evgenii Petukhov <petukhoven@gmail.com>
2025-03-16 19:23:51 +0100

    AWS S3: support MongoDB

--- LetsTalk.Server.Persistence.MongoDB.Services/MappingProfiles/MessageProfile.cs
+++ LetsTalk.Server.Persistence.MongoDB.Services/MappingProfiles/MessageProfile.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using LetsTalk.Server.Persistence.AgnosticServices.Models;
+using LetsTalk.Server.Persistence.Enums;
 using LetsTalk.Server.Persistence.MongoDB.Models;
 
 namespace LetsTalk.Server.Persistence.MongoDB.Services.MappingProfiles;
@@ -8,6 +9,7 @@ public class MessageProfile : Profile
 {
     public MessageProfile()
     {
-        CreateMap<Message, MessageServiceModel>();
+        CreateMap<Message, MessageServiceModel>()
+            .ForMember(x => x.FileStorageTypeId, x => x.MapFrom(source => source.Image != null ? source.Image.FileStorageTypeId : (int)FileStorageTypes.Local)); ;
     }
 }

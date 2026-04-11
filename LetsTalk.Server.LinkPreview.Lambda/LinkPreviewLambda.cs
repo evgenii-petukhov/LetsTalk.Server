bbd172e4c86aac45e3c4733ca91f9ed4b6f6015e
Evgenii Petukhov <petukhoven@gmail.com>
2026-04-11 21:42:33 +0200

    Service to lambda migration: added logging

--- LetsTalk.Server.LinkPreview.Lambda/LinkPreviewLambda.cs
+++ LetsTalk.Server.LinkPreview.Lambda/LinkPreviewLambda.cs
@@ -1,4 +1,5 @@
 using Amazon.Lambda.Core;
+using System.Text.Json;
 using LetsTalk.Server.LinkPreview.Utility.Abstractions.Models;
 using LetsTalk.Server.LinkPreview.Utility.Services;
 
@@ -8,14 +9,24 @@ namespace LetsTalk.Server.LinkPreview.Lambda
 {
     public static class LinkPreviewLambda
     {
-        public static Task<LinkPreviewResponse> GenerateAsync(LinkPreviewRequest request)
+        public static Task<LinkPreviewResponse> GenerateAsync(object request, ILambdaContext context)
         {
+            var serializedRequest = JsonSerializer.Serialize(request);
+            context.Logger.LogLine($"Request received: {serializedRequest}");
+
+            var linkPreviewRequest = request as LinkPreviewRequest;
+
+            if (linkPreviewRequest == null)
+            {
+                return null!;
+            }
+
             var downloadService = new DownloadService(new FakeHttpClientService());
             var linkPreviewService = new LinkPreviewService(
                 downloadService,
                 new RegexService(),
                 new FallbackLinkPreviewService(downloadService));
-            return linkPreviewService.GenerateLinkPreviewAsync(request);
+            return linkPreviewService.GenerateLinkPreviewAsync(linkPreviewRequest);
         }
     }
 }
